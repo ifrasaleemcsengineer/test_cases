22bfd178ff6a61874afcebc32bad6b43e3545d99
Ifra Saleem (2303-00-KHI-DEG) <124856222+ifra-saleem@users.noreply.github.com>
2023-10-08 23:30:50 +0500

    add

--- app.py
+++ app.py
@@ -2,23 +2,21 @@ import streamlit as st
 from dotenv import load_dotenv
 from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
-from langchain.embeddings import OpenAIEmbeddings, HuggingFaceInstructEmbeddings
+from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
 from langchain.chat_models import ChatOpenAI
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
 from htmlTemplates import css, bot_template, user_template
-from langchain.llms import HuggingFaceHub
 from langchain.prompts import PromptTemplate
 from langchain.prompts.chat import SystemMessagePromptTemplate
 import re
 
 
-def get_pdf_text(pdf_docs):
+def get_text_from_pdf(pdf_docs):
     text = ""
     for pdf in pdf_docs:
         pdf_reader = PdfReader(pdf)
-        # Check if the PDF contains the required heading
         first_page_text = pdf_reader.pages[0].extract_text()
         if "Software Requirements Specification" in first_page_text or "SRS" in first_page_text:
             for page in pdf_reader.pages:
@@ -27,6 +25,34 @@ def get_pdf_text(pdf_docs):
             st.error("Please upload a Software Requirements Specification(SRC) file")
             return None
     return text
+
+
+
+def get_text_from_txt(txt_docs):
+    text = ""
+    for txt in txt_docs:
+        text += txt.getvalue().decode('utf-8') + "\n"
+
+        # Check if the TXT contains the required heading
+        if "Software Requirements Specification" not in text and "SRS" not in text:
+            st.error("Please upload a Software Requirements Specification(SRC) file")
+            return None
+
+    return text
+
+def get_text_content(uploaded_files):
+    text = ""
+    for file in uploaded_files:
+        if file.type == 'application/pdf':
+            text += get_text_from_pdf([file]) or ""
+    
+        elif file.type == 'text/plain':
+            text += get_text_from_txt([file]) or ""
+        else:
+            st.error(f"File format '{file.type}' is not supported. Please upload a PDF, DOCX, or TXT file.")
+            return None
+    return text
+
 
 
 def get_text_chunks(text):
@@ -50,8 +76,7 @@ def get_conversation_chain(vectorstore):
     general_system_template = r"""
     {context}
     {question}
-    
-You are a chatbot responsible for generating all the possible test cases with their codes based on a user's input user story and the associated Software Requirements Specification (SRS) file. Your goal is to ensure that all possible test scenarios are covered.
+Generate all the possible test cases in proper format with their codes based on a user's input user story and the associated Software Requirements Specification (SRS) file. Your goal is to ensure that all possible test scenarios are covered.
 Please follow the format below when generating test cases:
 
 Example User Story:
@@ -109,8 +134,8 @@ Fail Criteria:
 - The product is not added to the shopping cart.
 - An error message is displayed indicating the addition was unsuccessful.
 
-Now, using the provided user story and any relevant information from the attached SRS file, your task is to generate all possible test cases in the format described above. Ensure that each test case is clear, specific, and covers various scenarios related to the user story. And generate the selenium code (python) for the test steps of the each possible test cases with he heading Selenium Code (Python).
-Generate all the test cases.
+Now, using the provided user story and any relevant information from the attached SRS file, your task is to generate all possible test cases in the format described above. Ensure that each test case is clear, specific, and covers various scenarios related to the user story. And generate the selenium code (python) for the test steps of the each possible test cases with the heading Selenium Code (Python).Find_element_by_xpath is deprecated. Please use find_element(by=By.XPATH, value=xpath) instead.
+Automatically determine the path to the WebDriver executable and support multiple browsers (chrome, firefox, and microsoft edge) using the webdriver_manager package in Python.
 """
     memory = ConversationBufferMemory(
         memory_key='chat_history', return_messages=True)
@@ -126,39 +151,58 @@ Generate all the test cases.
 
 
 def handle_userinput(user_question):
-    import re
-
     if st.session_state.conversation is not None:
-        response = st.session_state.conversation({'question': user_question})
-        print(response)
-        # Split the response into test cases and code blocks
-        pattern = r'Test Case \d+:(.*?)Selenium Code \(Python\):\n```python\n(.*?)```'
-        matches = re.findall(pattern, response['answer'], re.DOTALL)
+        with st.spinner("Generating Response..."):  
+            response = st.session_state.conversation({'question': user_question})
+            pattern = r'Test Case (\d+):([^`]+)Selenium Code \(Python\):\n```python\n(.*?)```'
+            matches = re.findall(pattern, response['answer'], re.DOTALL)
+            all_test_cases = []
 
-        for idx, match in enumerate(matches):
-            test_case = match[0].strip()
-            test_case_code = match[1].strip()
+            for idx, match in enumerate(matches):
+                test_case_number = match[0].strip()
+                test_case = match[1].strip()
+                test_case_code = match[2].strip()
+                file_name = f'test_case_code_{test_case_number}.py'
+                with open(file_name, 'w') as file:
+                    file.write(test_case_code)
+              
+                all_test_cases.append((test_case_number, test_case))
 
-            # Save the test case code to a separate file
-            file_name = f'test_case_code_{idx + 1}.py'
-            with open(file_name, 'w') as file:
-                file.write(test_case_code)
-            try:
-                exec(open(file_name).read())
-            except Exception as e:
-                st.error(f"Error executing saved Selenium code: {str(e)}") 
+            # Display the test cases with formatted titles (bold)
+            st.write("Generated Test Cases:")
+            for test_case_number, test_case in all_test_cases:
+                formatted_test_case = user_template.replace("{{MSG}}", f'<b>Test Case {test_case_number}: {test_case}</b>\n')
+                st.write(formatted_test_case, unsafe_allow_html=True)
+                try:
+                    exec(open(file_name).read())
+                except Exception as e:
+                    st.error(f"Error executing saved Selenium code: {str(e)}")
 
-            # Display the test case without code
-            formatted_test_case = user_template.replace("{{MSG}}", f'{test_case}\n')
-            st.write(formatted_test_case, unsafe_allow_html=True)
-
+            # Create a download button for all the test cases with bold headings
+            all_test_cases_text = "\n\n".join([f'Test Case {test_case_number}: {test_case}\n' for test_case_number, test_case in all_test_cases])
+            
+            st.download_button(
+                label="Save Test Cases",
+                data=all_test_cases_text,
+                file_name="all_test_cases.txt",  
+                mime="text/plain",  
+            )
     else:
         st.error("Please upload an SRS document.")
 
-   
+
+
+
         
 def main():
     load_dotenv()
+    custom_css = """
+<style>
+.center-align {
+    text-align: center;
+}
+</style>
+"""
     st.set_page_config(page_title="Test Cases Generation",
                        page_icon=":magic_wand:")
     st.write(css, unsafe_allow_html=True)
@@ -166,30 +210,25 @@ def main():
         st.session_state.conversation = None
     if "chat_history" not in st.session_state:
         st.session_state.chat_history = None
-    st.header("Story2Test: Streamlining User Story to Test Case Conversion:")
+    st.markdown(custom_css, unsafe_allow_html=True)
+    st.header("Story2Test: Streamlining User Story to Test Case Conversion")
+    uploaded_files = st.file_uploader("Upload your SRS document:", accept_multiple_files=True)
+
+    if uploaded_files:
+        with st.spinner("Processing"):
+            for uploaded_file in uploaded_files:
+                text = get_text_content([uploaded_file])
+                if text:
+                    text_chunks = get_text_chunks(text)
+                    vectorstore = get_vectorstore(text_chunks)
+                    st.session_state.conversation = get_conversation_chain(
+                            vectorstore)
     with st.form(key='user_input_form'):
-        user_question = st.text_input("Enter a user story according to your SRS:")
-        generate_button = st.form_submit_button("Generate Response")
+        user_question = st.text_area("Enter a user story according to your SRS:")
+        generate_button = st.form_submit_button("Generate Test Cases")
     if generate_button:
         if user_question:
             handle_userinput(user_question)
-    with st.sidebar:
-        st.subheader("Your SRS document")
-        pdf_docs = st.file_uploader(
-            "Upload your SRS document here", accept_multiple_files=True)
-        if pdf_docs:
-            for pdf in pdf_docs:
-                if pdf.type != 'application/pdf':
-                    st.error("File type is not valid. Please upload a PDF file.")
-                    return
-                with st.spinner("Processing"):
-                    raw_text = get_pdf_text(pdf_docs)
-                    if raw_text is not None:
-                        text_chunks = get_text_chunks(raw_text)
-                        vectorstore = get_vectorstore(text_chunks)
-                        st.session_state.conversation = get_conversation_chain(
-                            vectorstore)
-                        
-                        
+    
 if __name__ == '__main__':
     main()
--- htmlTemplates.py
+++ htmlTemplates.py
@@ -4,10 +4,10 @@ css = '''
     padding: 1.5rem; border-radius: 0.5rem; margin-bottom: 1rem; display: flex
 }
 .chat-message.user {
-    background-color: #2b313e
+    background-color: #d2d6e2
 }
 .chat-message.bot {
-    background-color: #475063
+    background-color: #d4d8e4
 }
 .chat-message .avatar {
   width: 20%;
@@ -23,8 +23,72 @@ css = '''
   padding: 0 1.5rem;
   color: #fff;
 }
-'''
+.block-container{
+    max-width: 70rem;
+}
+body {
+    max-width: 2200px;
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+}
+#MainMenu {visibility: hidden;}
+footer {visibility: hidden;}
+[data-testid="stAppViewContainer"]{
+    background-image: url('https://img.freepik.com/vecteurs-libre/concept-transformation-numerique-vecteur-fond-cerveau-technologie-ia_53876-117820.jpg?w=2000');
+    background-size: cover;
+}
+[data-testid="stHeader"]{
+     display: none;
+}
+span.css-10trblm.e16nr0p30{
+    text-align: center;
+    margin-bottom:70px;
+}
 
+
+.css-12ttj6m.epcbefy1 {
+    margin-top: 30px;
+}
+
+.css-12ttj6m{
+    border: none;
+}
+
+.css-184tjsw p {
+      font-size: 20px;
+}
+
+.element-container.css-kxz5b8.e1tzin5v3 {
+        margin-left: -15px;
+        
+}
+
+section.css-po3vlj.exg6vvm15 {
+        width: 1060px;
+}
+textarea.st-bf.st-c0.st-c1.st-c2.st-c3.st-c4.st-c5.st-c6.st-c7.st-c8.st-c9.st-b8.st-ca.st-cb.st-cc.st-cd.st-ce.st-cf.st-cg.st-ch.st-ae.st-af.st-ag.st-ci.st-ai.st-aj.st-bz.st-cj.st-ck.st-cl.st-cm.st-cn.st-co {
+    min-height: 200px;
+}
+
+.chat-message .message {
+    color: #303030;
+}
+
+button.css-1x8cf1d.edgvbvh5{
+    background-color: #d4d8e4;
+    border-radius: 0px;
+}
+
+button.css-1x8cf1d.edgvbvh5:hover{
+    border-color: #d4d8e4;
+    color: #000000;
+}
+
+button.css-1x8cf1d.edgvbvh5:active{
+    border-color: #d4d8e4;
+    color: #000000;
+}
+'''
 bot_template = '''
 <div class="chat-message bot">
     <div class="avatar">
@@ -33,12 +97,11 @@ bot_template = '''
     <div class="message">{{MSG}}</div>
 </div>
 '''
-
 user_template = '''
 <div class="chat-message user">
     <div class="avatar">
         <img src="https://us.123rf.com/450wm/blankstock/blankstock1605/blankstock160502581/56305912-speech-bubbles-icon-chat-or-blogging-sign-communication-symbol-orange-circle-button-with-icon-vector.jpg?ver=6" style="max-height: 40px; max-width: 40px; border-radius: 50%; object-fit: cover;">
-    </div>    
+    </div>
     <div class="message">{{MSG}}</div>
 </div>
 '''
